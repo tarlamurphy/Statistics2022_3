866b45de7919cf133952bbf892089d4716179693
tarlamurphy <114755762+tarlamurphy@users.noreply.github.com>
2023-01-05 18:22:29 +0000

    Update r_code_stats_project.R

--- r_code_stats_project.R
+++ r_code_stats_project.R
@@ -11,15 +11,28 @@
 # alternatively from data.world at https://data.world/us-doi-gov/581f789d-efa1-45db-a542-4812bed49004
 # Data is made available by the Department of the Interior, US Government
 
+
 ### Data Upload
 # set working directory, eg. setwd("~/R_Stats")
 
 polar <- read.csv("polar.csv", header = TRUE)
 head(polar)
 
+# Initial Data Clean-up
+
 typeof(polar$Age) #result is character
 typeof(polar$Total_Length) #appears that the variables are all characters
 
-as.integer(polar$Age) # turn the variable from a character to an integer
-as.integer(polar$Total_Length)
-polar$Age # that didnt work - still a character
+age <- as.double(polar$Age) # turn the variable from a character to an integer
+length <- as.double(polar$Total_Length) # must assign it to an object
+mass <- as.double(polar$Mass)
+cubs <- as.double(polar$Number_of_cubs)
+heart <- as.double(polar$Heart_Girth)
+skull <- as.double(polar$Skull_Width)
+
+# subset to remove excess columns
+polar1 <- subset(polar, select = c("Collecting_Agency","Population", "BearID", "Sex", "Age", "Total_Length",
+                 "Heart_Girth", "Skull_Width", "Mass", "Number_of_cubs"))
+
+summary(polar1)
+# 3174 entries
