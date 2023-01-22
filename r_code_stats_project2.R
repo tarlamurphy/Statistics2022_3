b370a53e6a78daa44ba357a1a132e4259ac7689a
lolaneuert <114755755+lolaneuert@users.noreply.github.com>
2023-01-22 19:57:19 +0100

    Update r_code_stats_project2.R

--- r_code_stats_project2.R
+++ r_code_stats_project2.R
@@ -56,39 +56,50 @@ polar4 <- na.omit(polar3) # omit all NAs
                          
 summary(polar4) # now all NAs are gone
 
+### to do: 
+#  - comment on data: what is the data set about and where does it come from? How many observations and variables are there? Which are the names and types of your variables? What do they represent? 
+#  - clean data: removing NAs, changing columns’ names and/or types, creating new columns, removing unnecessary columns, filtering observations, creating additional data frames/matrices/lists/vectors
+#  - do an EDA (explorative data analysis): calculate summary statistics of your variables and visualize their univariate and bivariate distributions
+#  - formulate at least an hypothesis and test it. You should comment the results of your test(s) and say if your hypothesis is supported or not by your data.
+
+### graphs ?
+# histogram of average number of accompanying cubs per mature female
+# what is the age of the females in correlation to their number of cubs
+# function of age and length: show growing phase
+# age distribution 
+# skull width correlation to sex
+# heart girth correlation to age and sex --> if we figure out how they measured the hearts
+# mass correlation with sex
+
 #  simple graphs
-
-# simple graphs
-
-# density plot sex vs age
-polar4 %>% 
+dens_age + dens_mass + dens_length +
+  box_age + box_mass + box_length
+  
+dens_age <- polar4 %>% 
   ggplot() +
   geom_density(aes(x = age, fill = sex), color = "black", alpha = 0.3) +
-  labs(title = "Polarbear age distribution for females and males", 
+  labs(title = "Polarbear age distribution for females & males", 
        subtitle = "Measurements from Chukchi and Southern Beaufort seas, 1981 - 2017", 
        x = "Age", y = " Density", fill = "Sex") +
   theme(panel.background = element_rect(fill = "white"))
 
-# density plot sex vs mass
-polar4 %>% 
+dens_mass <- polar4 %>% 
   ggplot() +
   geom_density(aes(x = mass, fill = sex), color = "black", alpha = 0.3) + 
-  labs(title = "Polarbear mass distribution for females and males", 
+  labs(title = "Polarbear mass distribution for females & males", 
        subtitle = "Measurements from Chukchi and Southern Beaufort seas, 1981 - 2017", 
        x = "Mass (in kg)", y = " Density", fill = "Sex") +
   theme(panel.background = element_rect(fill = "white"))
 
-# density plot sex vs total length
-polar4 %>% 
+dens_length <- polar4 %>% 
   ggplot() +
   geom_density(aes(x = total_length, fill = sex), color = "black", alpha = 0.3) +
   labs(title = "Polarbear length distribution for females and males", 
        subtitle = "Measurements from Chukchi and Southern Beaufort seas, 1981 - 2017", 
-       x = "Total Length", y = " Density", fill = "Sex") +
+       x = "Total Length (in cm)", y = " Density", fill = "Sex") +
   theme(panel.background = element_rect(fill = "white"))
 
-# boxplot sex vs age
-polar4 %>% 
+box_age <- polar4 %>% 
   ggplot() +
   geom_boxplot(aes(x = sex, y  = age)) +
   labs(title = "Polarbear age distribution for females and males", 
@@ -96,8 +107,7 @@ polar4 %>%
        x = "Sex", y = "Age (in years)") +
   theme(panel.background = element_rect(fill = "white"))
 
-# boxplot sex vs mass
-polar4 %>% 
+box_mass <- polar4 %>% 
   ggplot() +
   geom_boxplot(aes(x = sex, y  = mass)) +
   labs(title = "Polarbear mass distribution for females and males", 
@@ -105,15 +115,15 @@ polar4 %>%
        x = "Sex", y = "Mass (in kg)") +
   theme(panel.background = element_rect(fill = "white"))
 
-# boxplot sex vs total length
-polar4 %>% 
+box_length <- polar4 %>% 
   ggplot() +
   geom_boxplot(aes(x = sex, y  = total_length)) +
   labs(title = "Polarbear length distribution for females and males", 
        subtitle = "Measurements from Chukchi and Southern Beaufort seas, 1981 - 2017", 
        x = "Sex", y = "Length (in cm)") +
   theme(panel.background = element_rect(fill = "white"))
-#geom_violin for sex corelating to total length but no numbers (better boxplot)
+
+#geom_violin for sex correlating to total length but no numbers (better boxplot)
  polar4 %>% 
        ggplot(aes(x = sex, y = total_length)) +
        geom_violin()
@@ -154,22 +164,6 @@ polar4 %>%
 +     ggplot() +
 +     geom_violin(aes(x = sex, y = age, color = sex, fill = sex))
 
-### to do: 
-#  - comment on data: what is the data set about and where does it come from? How many observations and variables are there? Which are the names and types of your variables? What do they represent? 
-#  - clean data: removing NAs, changing columns’ names and/or types, creating new columns, removing unnecessary columns, filtering observations, creating additional data frames/matrices/lists/vectors
-#  - do an EDA (explorative data analysis): calculate summary statistics of your variables and visualize their univariate and bivariate distributions
-#  - formulate at least an hypothesis and test it. You should comment the results of your test(s) and say if your hypothesis is supported or not by your data.
-
-### graphs: 
-# histogram of average number of accompanying cubs per mature female
-# what is the age of the females in correlation to their number of cubs
-# function of age and length: show growing phase
-# age distribution 
-# skull width correlation to sex
-# heart girth correlation to age and sex --> if we figure out how they measured the hearts
-# mass correlation with sex
-# 
-
 
 # histogram of average number of accompanying cubs per mature female
 install.packages("naniar")
@@ -190,5 +184,44 @@ polar5 <- polar4 %>%
   filter(cubs_male30 != "NA") # filter out the NAs so we only have rows for mature females
 
 colnames(polar5)[2] <- "cubs" # change the column names for cubs
+
+# create hypothesis
+
+# our first hypothesis concerns the correlation between Total length of Polarbears and their Mass
+# H0 is stated as: Total length and Mass are not positively correlated,
+# and H1 is stated as: Total length and Mass are positively correlated
+# to test this we use the function cor.test, and specify greater as the alternative
+cor.test(polar4$total_length, polar4$mass, alternative = "greater")
+# the Pearson's product-moment correlation shows a p-value of less than 0.05, meaning the correlation is significant
+# the correlation value is 0.86, showing a strong positive correlation between length and mass of polarbears
+# this means we can reject H0 and accept H1
+
+# create plot to show correlation, with geom_smooth adding the correlation as a linear model??
+ggplot(data = polar4) + 
+  geom_point(aes(x = total_length, y = mass), alpha = 0.3) + 
+  geom_smooth(aes(x = total_length, y = mass), method = "lm") +
+  labs(title = "Polarbear length and mass correlation", 
+       subtitle = "Measurements from Chukchi and Southern Beaufort seas, 1981 - 2017", 
+       x = "Total Length (in cm)", y = "Mass (in kg)") +
+  theme(panel.background = element_rect(fill = "white"))
+
+# our second hypothesis tries to correlate the number of cubs in mature female polarbears with their mass
+# H0 is stated as: there is no positive correlation between number of cubs and mass 
+# H1 is stated as: there is a positive correlation between number of cubs and mass
+# we again use function cor.test
+cor.test(polar5$cubs, polar5$mass, alternative = "greater")
+# this time the Pearson's correlation gave a p-value of 0.9
+# as this is not smaller than 0.05, the test of correlation was not significant
+# the correlation comes out to be -0.16
+# we therefore can neither reject nor accept H1
+
+# now we create a plot that shows the non-significant correlation
+ggplot(data = polar5) + 
+  geom_point(aes(x = cubs, y = mass), alpha = 0.3) + 
+  geom_smooth(aes(x = cubs, y = mass), method = "lm") +
+  labs(title = "Mature female Polarbears: correlation of number of cubs and mass", 
+       subtitle = "Measurements from Chukchi and Southern Beaufort seas, 1981 - 2017", 
+       x = "Number of cubs", y = "Mass (in kg)") +
+  theme(panel.background = element_rect(fill = "white"))
 
 
